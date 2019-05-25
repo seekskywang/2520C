878460d4bce65eba1d0930dfa5763b1309051e41
seekskywang <seekskywang@gmail.com>
2019-05-25 14:10:27 +0800

    增加U盘升级

--- User/flash_if.h
+++ User/flash_if.h
@@ -5,9 +5,9 @@
 #include <stm32f4xx.h>
 #include "flash_if.h"
 
-#define USER_FLASH_FIRST_PAGE_ADDRESS 0x08000000 //Æ«ÒÆ128*2KB
-#define USER_FLASH_LAST_PAGE_ADDRESS  0x080A0000  //Íùºó0xA0000BÎªAPP´æ´¢Çø
-#define USER_FLASH_END_ADDRESS        0x080BFFFF  
+#define USER_FLASH_FIRST_PAGE_ADDRESS 0x080C0000 //Æ«ÒÆ128*2KB
+#define USER_FLASH_LAST_PAGE_ADDRESS  0x080E0000  //Íùºó0xA0000BÎªAPP´æ´¢Çø
+#define USER_FLASH_END_ADDRESS        0x080FFFFF  
 #define USER_FLASH_SIZE   (USER_FLASH_END_ADDRESS - USER_FLASH_FIRST_PAGE_ADDRESS)
 
 uint32_t FLASH_If_Write(__IO uint32_t* Address, uint32_t* Data, uint16_t DataLength);
